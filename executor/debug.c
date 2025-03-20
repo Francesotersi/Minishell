ce0018a349ed8f5af0d631430d44251a57389780
Alessandro Russo <alerusso@c1r4p4.42firenze.it>
2025-03-20 17:14:11 +0100

    debug non pronto

--- executor/debug.c
+++ executor/debug.c
@@ -6,21 +6,44 @@
 /*   By: alerusso <alerusso@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/19 12:08:08 by ftersill          #+#    #+#             */
-/*   Updated: 2025/03/19 17:02:06 by alerusso         ###   ########.fr       */
+/*   Updated: 2025/03/20 17:12:42 by alerusso         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 # include "debug.h"
+# include "debug_resources/all.h"
 
-int	main(void)
+int	testing(int test_num);
+
+int	main(int argc, char **argv)
 {
 	int		fd;
-	char	**matrix;
+	int		n;
+	char	*filename;
+	int		i;
+	char	*line;
 
-	fd = open("debug_resources/input_samples/1", O_CREAT | O_RDWR, 0666);
+	if (argv == NULL)
+		return (fd_printf(2, "\nInsert a file num.\n"));
+	filename = ft_strjoin("debug_resources/input_samples/", argv[1]);
+	if (!filename)
+		return (fd_printf(2, "\nMalloc failed\n"));
+	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0666);
 	if (fd == -1)
-		return (l_printf("Failed to open file"));
-	matrix = ft_split("\tContent1,\tSeparator,\tContent2,\tPunti_ferita,\
-	\tRayquaza,\tNumero_di_peni_preso", ',');
-	initiate_file(fd, "debug_resources/input_samples/1", matrix);
+		return (fd_printf(2, "Failed to open file"));
+	give_filedata(fd, filename);
+	n = num_line(1, "[Caso");
+	line = get_n_line(fd, n);
+	i = 0;
+	while (testing(i))
+		++i;
+	del_filedata();
+}
+
+int	testing(int test_num)
+{
+	char	***matrix;
+	char	*test;
+	
+	test = ft_strjoin
 }
--- executor/debug.h
+++ executor/debug.h
@@ -6,12 +6,13 @@
 /*   By: alerusso <alerusso@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/19 16:49:14 by alerusso          #+#    #+#             */
-/*   Updated: 2025/03/19 17:02:03 by alerusso         ###   ########.fr       */
+/*   Updated: 2025/03/20 14:50:52 by alerusso         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef DEBUG_H
 # define DEBUG_H
 # include "debug_resources/all.h"
+# include "debug_resources/libft.h"
 
 #endif
